c45dd19fe7a1fbf68f60f9563780f4c885be5504
hdhung11 <hdhung.an@gmail.com>
2025-03-25 22:38:24 +0700

    duchung

---
+++ Models/SanPhamPartial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LTW.Models
+{
+    public partial class SanPham : ICloneable // Thêm interface ICloneable
+    {
+        public object Clone()
+        {
+            return new SanPham //Tạo đối tượng mới,sao chép toàn bộ giá trị thuộc tính từ object gốc hiện tại (this).
+            {
+                TenSP = this.TenSP,
+                Hinh = this.Hinh,
+                GiaSP = this.GiaSP,
+                GiaVon = this.GiaVon,
+                SoLuongTon = this.SoLuongTon,
+                MoTa = this.MoTa,
+                MaLoai = this.MaLoai,
+                MaNCC = this.MaNCC,
+                TrangThai = this.TrangThai
+            };
+        }
+    }
+}
